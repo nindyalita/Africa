fa31fee478804477a3afae39274a6ef4477d49d7
nindyalita <118523607+nindyalita@users.noreply.github.com>
2023-10-21 11:26:02 +0700

    motion animation

---
+++ Africa/View/MotionAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  MotionAnimationView.swift
+//  Africa
+//
+//  Created by Nindya Alita Rosalia on 21/10/23.
+//
+
+import SwiftUI
+
+struct MotionAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MotionAnimationView()
+}
---
+++ Africa/View/coba.swift
@@ -0,0 +1,18 @@
+//
+//  coba.swift
+//  Africa
+//
+//  Created by Nindya Alita Rosalia on 16/10/23.
+//
+
+import SwiftUI
+
+struct coba: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    coba()
+}
